eaba80e30a5830f5bc13fe1886f62828271924b3
Genki-Sgi <sggk8787@gmail.com>
2022-07-23 01:18:39 +0900

    add space

--- js/puzzle.js
+++ js/puzzle.js
@@ -33,7 +33,7 @@ function click(e){
         swap(i, i - 4);
     }else if(i + 4 >= 0 && tiles[i + 4].value == 0){
         swap(i, i + 4);
-    }else if(i % 4 != 0 && tiles[i-1].value == 0){
+    }else if(i % 4 != 0 && tiles[i - 1].value == 0){
         swap(i, i - 1);
     }else if(i % 4 != 3 && tiles[i + 1].value == 0){
         swap(i, i + 1);
